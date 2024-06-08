850e70f11c71363ade084af2620cb33fa8f25c60
AntwnhsG <antns@outlook.com.com>
2024-06-08 21:13:33 +0300

    Added user roles according to keycloak
    Now the user is seeing pages of the application he is supposed to according to his role. Unauthorized users cannot enter any page without logging in.

--- web/src/components/layout/Layout.jsx
+++ web/src/components/layout/Layout.jsx
@@ -9,18 +9,35 @@ import { useEffect } from "react";
 
 const Layout = (props) => {
   const navigate = useNavigate();
+  const { keycloak } = props;
+  const role = null;
+
+  if (keycloak.authenticated) {
+    const token = keycloak.token;
+
+  // Decode the token to extract user information
+    const decodedToken = keycloak.tokenParsed;
+
+  // Extract user roles from the decoded token
+    const resourceAccess = decodedToken.resource_access;
+
+  // Assuming you have a specific client ID for your application
+    const role = resourceAccess['front-end-app'].roles[0];
+  console.log('User Role:', role);
+}
+
   //const role = localStorage.getItem("role");
   //console.log(role);
 
-//   useEffect(() => {
-//     if (localStorage.getItem("tin") === null) {
-//       navigate("/");
-//     }
-//   }, []);
+  useEffect(() => {
+    if (role) {
+      navigate("/");
+    }
+  }, []);
 
   console.log(localStorage.getItem("tin"));
   //if (localStorage.getItem("tin") != null) {
-    //if (role === "user") {
+    if (role === "user") {
       return (
         <div className="Layout">
           <Navbar />
@@ -31,17 +48,17 @@ const Layout = (props) => {
           </main>
         </div>
       );
-//     }else{
-//       return (
-//         <div className="Layout">
-//           <NavBarAdmin />
-//           <TabMenuAdmin />
-//           <main>
-//             <Outlet />
-//           </main>
-//         </div>
-//       );
-//     }
+    }else{
+      return (
+        <div className="Layout">
+          <NavBarAdmin />
+          <TabMenuAdmin />
+          <main>
+            <Outlet />
+          </main>
+        </div>
+      );
+    }
   //}
 };
 
